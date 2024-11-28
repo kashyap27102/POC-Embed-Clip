3b91cd2d189a98b498caa2c244332955060ae6ce
Kashyap27102 <ksnakrani5544@gmail.com>
2024-11-28 16:00:58 +0530

    change route

---
+++ app/embed/track/[clipId]/page.tsx
@@ -0,0 +1,30 @@
+import MusicPlayerCard from "@/app/components/MusicPlayerCard";
+import { Metadata } from "next";
+import React from "react";
+
+export const metadata: Metadata = {
+  openGraph: {
+    title: "Song title",
+    description: "Song description",
+
+    images: [
+      {
+        url: "https://poc-embed-clip.vercel.app/api/og",
+        width: 800,
+        height: 600,
+      },
+    ],
+    // type: "music.song",
+    // audio: "https://www2.cs.uic.edu/~i101/SoundFiles/BabyElephantWalk60.wav",
+  },
+};
+
+const page = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <MusicPlayerCard />
+    </div>
+  );
+};
+
+export default page;
