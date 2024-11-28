e854d004e928414e65f711c94dde49cd66e1b2da
Kashyap27102 <ksnakrani5544@gmail.com>
2024-11-28 16:12:14 +0530

    update og image content

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -1,3 +1,4 @@
+import MusicPlayerCard from "@/app/components/MusicPlayerCard";
 import { ImageResponse } from "next/og";
 // App router includes @vercel/og.
 // No need to install it.
@@ -5,27 +6,8 @@ import { ImageResponse } from "next/og";
 export const runtime = "edge";
 
 export async function GET() {
-  return new ImageResponse(
-    (
-      <div
-        style={{
-          fontSize: 40,
-          color: "black",
-          background: "white",
-          width: "100%",
-          height: "100%",
-          padding: "50px 200px",
-          textAlign: "center",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        👋 Hello
-      </div>
-    ),
-    {
-      width: 1200,
-      height: 300,
-    }
-  );
+  return new ImageResponse(<MusicPlayerCard />, {
+    width: 1200,
+    height: 300,
+  });
 }
